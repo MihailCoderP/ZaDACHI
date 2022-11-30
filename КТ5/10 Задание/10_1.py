763a091fa49ee4873e3a20804975c4215bd81a10
MihailCoderIDLE <114363594+MihailCoderP@users.noreply.github.com>
2022-11-30 18:18:46 +0300

    10

---
+++ КТ5/10 Задание/10_1.py
@@ -0,0 +1,22 @@
+import json
+file = open('input 10.1.json', 'r')
+arr = json.load(file)
+file.close()
+n = int(len(arr) ** 0.5)
+m = (1 + n) * n / 2
+table = [[None for j in range(n)] for i in range(n)]
+j, counter = 0, 0
+for i in range(n):
+  k = 0
+  while(k < j):
+    table[i][k] = table[k][i]
+    k += 1
+  while(k < n):
+    table[i][k] = arr[counter]
+    k += 1
+    counter += 1
+  j += 1
+file = open('output.json', 'w')
+json.dump(arr, file)
+file.close()
+print(table)
---
+++ КТ5/10 Задание/10_2.py
@@ -0,0 +1,17 @@
+import json
+file = open('input 10.2.json', 'r')
+arr = json.load(file)
+n = len(arr)
+file.close()
+diag = []
+trace = 0
+for i in range(n):
+  trace += arr[i][i]
+for i in range(n):
+  for j in range(n):
+    if i % 2 == 0:
+      arr[i][j] /= trace
+file = open('output.json', 'w')
+json.dump(arr, file)
+file.close()
+print(arr)
